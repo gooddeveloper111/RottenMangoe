563eebf117d0d6b81fe71bca775e18bab4627498
gooddeveloper111 <payman1234567@gmail.com>
2016-08-19 23:03:56 +0000

    Generating review controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
-# config/routes.rb
-    Rails.application.routes.draw do
-  get 'sessions/new'
+RottenMangoes::Application.routes.draw do
 
-  get 'sessions/create'
-
-      resources :movies
+      resources :movies do
+        resources :reviews, only: [:new, :create]
+      end
       resources :users, only: [:new, :create]
-      resources :sessions, only: [:new, :create]
       resources :sessions, only: [:new, :create, :destroy]
-    end
 
+    end
